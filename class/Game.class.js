efb414c6aa9fc8a8cee38726dd2a60b06ede0e45
Yonathan <yonathan.piat@gmail.com>
2022-08-13 16:38:26 +0200

    modif uml & ajout constructeur de game

---
+++ class/Game.class.js
@@ -0,0 +1,12 @@
+function Game() {
+    this._Players = [null,null];
+    
+    this._playerWhoPLay = -1;
+    
+    this._cells = [
+                    [-1,-1,-1],
+                    [-1,-1,-1],
+                    [-1,-1,-1]
+                  ];
+}
+
