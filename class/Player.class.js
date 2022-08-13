ddbe2281b1cfa0185dc56d9dd63b8f53a1b8d0bd
JustineM <justine.merlin94@gmail.com>
2022-08-13 16:07:22 +0200

    Player class skeleton

---
+++ class/Player.class.js
@@ -0,0 +1,22 @@
+/**
+ * Player class.
+ *
+ * @constructor
+ * @param {String} name - name of a person.
+ * @param {String} session  - session.
+ * @param {Object} icon  - icon of player.
+ */
+
+function Player(nickname, session, icon) {
+    this._nickname = nickname;
+    this._session = session;
+    this._icon = icon;
+}
+
+Player.prototype.changeIcon = function () {
+
+}
+
+Player.prototype.changeNickname = function () {
+
+}
