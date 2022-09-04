166b1d6c279b6cdb8f1216fd615616c80deaf66c
Yonathan <yonathan.piat@gmail.com>
2022-09-04 22:34:10 +0200

    add skeleton game

--- class/Game.class.js
+++ class/Game.class.js
@@ -9,3 +9,54 @@ function Game() {
                     [-1,-1,-1]
                   ];
 }
+
+Game.prototype.editCell = function (coordinate) {
+
+}
+
+Game.prototype.winCondition = function (coordinate) {
+
+}
+
+Game.prototype.checkRow = function (coordinate) {
+
+}
+
+Game.prototype.checkCol = function (coordinate) {
+
+}
+
+Game.prototype.checkRDiagno = function (coordinate) {
+
+}
+
+Game.prototype.reset = function () {
+
+}
+
+Game.prototype.checkIfCellIsPlayable = function (coordinate ,player) {
+
+}
+
+Game.prototype.changeTurn = function () {
+
+}
+
+
+
+Game.prototype.checkIfPlayerIsMissing = function () {
+
+}
+
+
+Game.prototype.surrender = function (player) {
+
+}
+
+Game.prototype.play = function (coordinate) {
+
+}
+
+Game.prototype.addPlayer = function (player) {
+
+}
